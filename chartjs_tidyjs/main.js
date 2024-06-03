8ffcf7356576a09f55a4bce8ae8f2d31bcc371d8
Frank Guarino <fguarino71@gmail.com>
2024-06-02 20:16:17 -0400

    tidyjs chartjs wrap up

--- chartjs_tidyjs/main.js
+++ chartjs_tidyjs/main.js
@@ -1,4 +1,5 @@
 import Chart from 'chart.js/auto'
+import { tidy, filter, count, groupBy, mutate, rename, sort, desc } from '@tidyjs/tidy'
 
 
 let chart = new Chart('chart1', {
@@ -48,3 +49,55 @@ let chart = new Chart('chart1', {
 })
 // chart.canvas.parentNode.style.height = '1280px';
 // chart.canvas.parentNode.style.width = '1280px';
+
+function tidyjs_stuff(data) {
+
+    let region_summary = tidy(
+        data,
+        count('region'),
+        //sort(desc('n'))
+        sort('region')
+    )
+
+    let list = '';
+    region_summary.forEach(d =>{
+        list += '<div>' + d.region + ' (' + d.n + ') ' + '</div>'
+    })
+    console.log(region_summary)
+    document.querySelector('#output').innerHTML = list;
+
+    let chart = new Chart('chart2', {
+        type:'bar',
+        data: {
+            // DO NOT REARRANGE DATA HERE. DECONSTRUCTION OF DATA BETWEEN LABELS AND DATA
+            // ALLOWS FOR UNRELATED DATA TO MATCH UP I.E. SCOTLAND NOT GOING WITH 1666.
+            labels: region_summary.map((d, i) => d.region),
+            datasets:[
+                {
+                    data: region_summary.map((d, i) => d.n)
+                }
+            ]
+        },
+        options:{
+            
+            responsive:true, 
+        
+            plugins:{
+                title:{
+                    display: true,
+                    text: "Researches by Region"
+                },
+                legend:{
+                    display: false
+                }
+            }
+        }
+    })
+    
+    return chart
+}
+
+fetch('./repd.json')
+  .then(res => res.json())
+  .then(data => tidyjs_stuff(data));
+
