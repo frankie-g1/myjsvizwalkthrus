994d215281137168485ee7132411c7fba4f77246
Frank Guarino <fguarino71@gmail.com>
2024-06-05 14:19:09 -0400

    tidy

--- d3-dot/main.js
+++ d3-dot/main.js
@@ -1,36 +1,56 @@
 import { select, selectAll } from "d3-selection"
 import { axisBottom, axisLeft } from "d3-axis"
-import { scaleLinear } from "d3-scale"
+import { scaleLinear, scaleTime } from "d3-scale"
 import { extent } from "d3-array"
+import { mutate, filter } from '@tidyjs/tidy'
 import Popup from '@flourish/popup'
-import uniq from 'lodash-es/uniq'
 
 function drawXAxis(data){
 
-    let years = uniq(data.map(d => d['yearSubmitted']))
-        .filter(d => d != '')
-        .sort()
+    let width = 1200, height = 600;
+    let leftMargin = 210, rightMargin = 20, topMargin = 20, bottomMargin = 40;
     
-
-    let scale = scaleLinear()
-        .domain(extent(years))
-        .range(0, 100)
-
+    let yearsExtent = extent(data, d=> d.dateSubmitted)
+    let xLength = width - leftMargin - rightMargin;
+    
+    let scale = scaleTime()
+        .domain(yearsExtent)
+        .range([0, xLength])
 
     let xaxis = axisBottom(scale)
 
-    select('.x-axis')
-        .call(xaxis)
+
+    select('svg .x-axis')
+        .attr('transform', 'translate(' + leftMargin + ',' + topMargin +')')
+        .call(xaxis);
+
+
+    se
 }
 
 
 
 function draw(data){
 
-    drawXAxis()
+    drawXAxis(data)
+}
+
+
+function initData(data){
+    
+    // Tidy is great because it filters, mutates, and sorts
+    // functions are done 'in place' memory
+    let data = tidy(
+        data,
+        filter(d => d.dateSubmitted != ''),
+        mutate({
+            dateSubmitted : d => new Date(d.dateSubmitted)
+        }),
+        sort(desc('capacity'))
+    )
 }
 
 
 fetch('./repd.json')
     .then(res => res.json())
-    .then(data => draw(data))
+    .then(data => initData(data))
